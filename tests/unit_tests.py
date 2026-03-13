5a5384a11cf439e1823050fd6ff92bde77afa639
cb9 <cbaie9@proton.me>
2026-03-13 15:03:45 +0100

    Adding test

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,6 +4,9 @@ from resa.resa import *
 class bookMeetingRoomUnitTests(unittest.TestCase):
   def test_small_room(self):
     self.assertEqual(bookMeetingRoom(5), Room.SMALL)
+    self.assertEqual(bookMeetingRoom(15), Room.MEDIUM)
+    self.assertEqual(bookMeetingRoom(35), Room.LARGE)
+    self.assertEqual(bookMeetingRoom(55), Room.REFUSE)
 
 if __name__ == '__main__':
   unittest.main()
