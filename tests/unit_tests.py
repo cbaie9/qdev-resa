46fe0945e748d7477e11c4c1959a8711568b1815
cb9 <cbaie9@proton.me>
2026-03-13 15:32:52 +0100

    Serveral function for tests

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -4,8 +4,11 @@ from resa.resa import *
 class bookMeetingRoomUnitTests(unittest.TestCase):
   def test_small_room(self):
     self.assertEqual(bookMeetingRoom(5), Room.SMALL)
+  def test_medium_room(self):
     self.assertEqual(bookMeetingRoom(15), Room.MEDIUM)
+  def test_large_room(self):
     self.assertEqual(bookMeetingRoom(35), Room.LARGE)
+  def test_refuse_room(self):
     self.assertEqual(bookMeetingRoom(55), Room.REFUSE)
 
 if __name__ == '__main__':
